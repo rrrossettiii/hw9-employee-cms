d6fb85bc69585d72cb441c7110a99e37444e5199
rrrossettiii <rrrossettiii@gmail.com>
2020-11-06 06:05:49 -0500

    updates to inquirer options, functions, and schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 -- DEPARTMENTS
 -- =============
-USE employee_DB;
+USE employee_db;
 INSERT INTO department (name) VALUES ('Sales');
 INSERT INTO department (name) VALUES ('Intern');
 INSERT INTO department (name) VALUES ('Warehouse');
@@ -8,17 +8,30 @@ INSERT INTO department (name) VALUES ('Accounting');
 INSERT INTO department (name) VALUES ('Management');
 INSERT INTO department (name) VALUES ('Human Resources');
 
+USE employee_db;
 SELECT * FROM department;
 
 -- ROLES
 -- =============
+USE employee_db;
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Paper Sales-Person', '10000', '1');
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Temp', '0', '2');
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Foreman', '187050', '3');
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Accountant', '137050', '4');
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Office Administrator', '15000', '5');
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Asst. to the RM', '45700', '5');
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Regional Manager', '82300', '5');
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Receptionist', '8000', '6');
+INSERT INTO employeeRole (title, salary, department_id) VALUES ('Human Relations', '227050', '6');
+
 USE employee_DB;
-INSERT INTO role (title, salary, department_id) VALUES ('Paper Sales-Person', '10000', '1');
-INSERT INTO role (title, salary, department_id) VALUES ('Temp', '0', '2');
-INSERT INTO role (title, salary, department_id) VALUES ('Foreman', '187050', '3');
-INSERT INTO role (title, salary, department_id) VALUES ('Accountant', '137050', '4');
-INSERT INTO role (title, salary, department_id) VALUES ('Office Administrator', '15000', '5');
-INSERT INTO role (title, salary, department_id) VALUES ('Asst. to the RM', '45700', '5');
-INSERT INTO role (title, salary, department_id) VALUES ('Regional Manager', '82300', '5');
-INSERT INTO role (title, salary, department_id) VALUES ('Receptionist', '8000', '6');
-INSERT INTO role (title, salary, department_id) VALUES ('Human Relations', '227050', '6');
+SELECT * FROM employeeRole;
+USE employee_DB;
+SELECT title
+    FROM employeeRole
+    -- ORDER BY department_id in ascending order, same with title
+    ORDER BY department_id ASC, title ASC;
+
+-- EMPLOYEES
+-- =============
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Scott', 9, 1);
--- server.js
+++ server.js
@@ -16,5 +16,155 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId);
-	runApp();
+	init();
 });
+
+// Prompts;
+// ============
+async function runPrompts() {
+	await inquirer
+		.prompt({
+			name: "option",
+			type: "rawlist",
+			message: "Select option:",
+			choices: [
+				"View employees",
+				"Add employees",
+				"Update employee roles",
+				"Update employee manager",
+				"Delete employees",
+				"View roles",
+				"Add roles",
+				"Delete roles",
+				"View departments",
+				"Add departments",
+				"Delete departments",
+			],
+		})
+		.then(
+			await function (response) {
+				switch (response.option) {
+					case "View employees":
+						viewEmployees();
+						break;
+
+					case "Add employees":
+						addEmployees();
+						break;
+
+					case "Delete employees":
+						deleteEmployees();
+						break;
+
+					case "Update employee roles":
+						updateEmployeeRoles();
+						break;
+
+					case "Update employee manager":
+						updateEmployeeManager();
+						break;
+
+					case "View roles":
+						viewRoles();
+						break;
+
+					case "Add roles":
+						addRoles();
+						break;
+
+					case "Delete roles":
+						deleteRoles();
+						break;
+
+					case "View departments":
+						viewDepartments();
+						break;
+
+					case "Add departments":
+						addDepartments();
+						break;
+
+					case "Delete Departments":
+						deleteDepartments();
+						break;
+				}
+			}
+		);
+}
+
+// Employee Functions;
+// =============
+// - View;
+async function viewEmployees() {
+	var query =
+		"SELECT employees.id, employees.first_name, employees.last_name, department.name AS department, role_table.title, role_table.salary FROM employees INNER JOIN role_table ON employees.role_id = role_table.role_id INNER JOIN department ON role_table.department_id = department.department_id";
+	connection.query(query, function (err, response) {
+		console.table(response);
+		runPrompts();
+	});
+}
+// - Add;
+async function addEmployees() {
+	await inquirer
+		.prompt(
+			{
+				name: "first_name",
+				type: "input",
+				message: "What is the employee's first name?",
+			},
+			{
+				name: "last_name",
+				type: "input",
+				message: "What is the employee's last name?",
+			},
+			{
+				name: "role",
+				type: "input",
+				message: "What is the employee's role?",
+			}
+		)
+		.then(async function (answer) {
+			connection.query(
+				`INSERT INTO role_table (title) VALUES ("${answer.rolesInput}");`,
+				function (err, response) {
+					console.log(response);
+					runPrompts();
+				}
+			);
+		});
+}
+// -Delete;
+async function deleteEmployees() {
+	var query =
+		'SELECT CONCAT(first_name , " ", last_name) AS name, id AS value FROM employee_DB.employees;';
+	connection.query(query, function (err, response) {
+		console.table(response);
+		inquirer
+			.prompt({
+				name: "delete",
+				type: "list",
+				choices: response,
+				message: "who would you like to delete",
+			})
+			.then(function (answer) {
+				let deleteID = answer.delete;
+
+				connection.query(
+					"DELETE FROM employees WHERE ?",
+					{
+						id: deleteID,
+					},
+					function (err, res) {
+						if (err) throw err;
+						console.log(res.affectedRows + " employee deleted!\n");
+						runPrompts();
+					}
+				);
+			});
+	});
+}
+
+// Role Functions;
+// =============
+
+runPrompts();
